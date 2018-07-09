aa393f9e77b4297d2706daee222cb0a4c93ac29e
Lahiru Tennakoon <lahiru@speralabs.com>
2018-07-09 16:47:56 +0530

    Added speech to text conversion parameters.

--- controllers/videoController_lt.js
+++ controllers/videoController_lt.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const cmd=require('node-cmd');
 const fs = require('fs');
-var ffmpeg = require('fluent-ffmpeg');
-var SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
+const ffmpeg = require('fluent-ffmpeg');
+const SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
 
 //Import model
 const videoModel = new require('../models/video_lt');
@@ -215,14 +215,45 @@ module.exports.getOneVideo = function (req, res) {
 module.exports.test = function (req, res) {
 
     // Create an object of SpeechToText
-    var speech_to_text = new SpeechToTextV1({
+    const speech_to_text = new SpeechToTextV1({
         "username": "c70e62af-7ac6-4b2b-8c03-2c29c90bca0a",
         "password": "ZsN5o7UPmqQi"
-});
-    /*var proc = new ffmpeg({ source: 'D:/SLIIT/Year4,Semester1/CDAP/Prototype/OBSStudioPlugin/RecordedVideos/[Stardust] Rule the world_HD.mp4', nolog: true });
+    });
+
+    const files = ['/tmp/02.002 What is Angular.mp3'];
+
+    for (let file in files)
+    {
+        const params = {
+            audio: fs.createReadStream(files[file]),
+            content_type: 'audio/mp3'
+        };
+
+        speech_to_text.recognize(params, function (error, transcript) {
+            if (error)
+            {
+                console.log('Error:'+  error);
+            }
+            else
+            {
+                console.log('success');
+                console.log(transcript);
+
+                for(let i=0; i<transcript.results.length; i++)
+                {
+                    console.log(transcript.results[i].alternatives[0].transcript);
+                }
+                // console.log(JSON.stringify(transcript, null, 2));
+                // console.log(transcript.results[0].alternatives[0].transcript);
+                //     console.log(transcript.results[1].alternatives[0].transcript);
+            }
+        });
+    }
+
+    /*const proc = new ffmpeg({ source: '/tmp/02.002 What is Angular.mp4', nolog: true });
 
 
-    proc.setFfmpegPath("F:/Program Files/ffmpeg-20180706-cced03d-win64-static/bin/ffmpeg.exe")
+    proc.setFfmpegPath("/usr/bin/ffmpeg")
     .toFormat('mp3')
 
         .on('end', function() {
@@ -232,6 +263,6 @@ module.exports.test = function (req, res) {
             console.log('an error happened: ' + err.message);
         })
         // save to file <-- the new file I want -->
-        .saveToFile('D:/SLIIT/Year4,Semester1/CDAP/Prototype/OBSStudioPlugin/RecordedVideos/[Stardust] Rule the world_HD.mp3');*/
-    console.log('done');
-}
+        .saveToFile('/tmp/02.002 What is Angular.mp3');
+    console.log('done');*/
+};
