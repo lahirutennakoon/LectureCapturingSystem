a886168e6e4946be0b5aa25f0b13c20727b867d3
ashen <jayasinghe.ashen@gmail.com>
2018-07-27 02:55:05 +0530

    analytics_v1

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 //Import libraries
-const express        = require('express');
-const bodyParser     = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
 const fileUpload = require('express-fileupload');
+const server = express();
+
+//SOCKET.IO
+var http = require('http').Server(server);
+var io = require('socket.io')(http);
+
 const cors = require('cors');
 const authCheckMiddleware = require('./middleware/authCheck_aj');
 var authRoute = require('./routes/auth_aj');
@@ -14,7 +19,8 @@ const config = require('./configurations/config');
 // Import the routes file
 const routes = require('./routes/routes');
 
-const server = express();
+var visitorsData = {};
+
 const serverPort = config.serverPort;
 const databaseUrl = config.databaseUrl;
 
@@ -26,23 +32,74 @@ server.use(cors());
 server.options('*', cors());
 
 // Connect to mongodb database
-mongoose.connect(databaseUrl, function(err){
-    if(err){
-        console.log('Error connecting to: '+ databaseUrl)
+mongoose.connect(databaseUrl, function (err) {
+    if (err) {
+        console.log('Error connecting to: ' + databaseUrl)
     }
-    else{
-        console.log('Connected to: '+ databaseUrl)
+    else {
+        console.log('Connected to: ' + databaseUrl)
     }
 });
 
 server.use('/user', authRoute);
 
+io.on('connection', function (socket) {
+    io.emit('updated-stats', computeStats());
+    socket.on('visitor-data', function (data) {
+        visitorsData[socket.id] = data;
+        io.emit('updated-stats', computeStats());
+    })
+    socket.on('disconnect', function () {
+        delete visitorsData[socket.id];
+        io.emit('updated-stats', computeStats());
+    });
+});
+
+function computeStats() {
+    var temp = {
+        pages: computePageCounts(),
+        referrers: computeRefererCounts(),
+        activeUsers: getActiveUsers()
+    };
+    return temp;
+}
+
+function computePageCounts() {
+    var pageCounts = {};
+    for (var key in visitorsData) {
+        var page = visitorsData[key].page;
+        if (page in pageCounts) {
+            pageCounts[page]++;
+        } else {
+            pageCounts[page] = 1;
+        }
+    }
+    return pageCounts;
+}
+
+function computeRefererCounts() {
+    var referrerCounts = {};
+    for (var key in visitorsData) {
+        var referringSite = visitorsData[key].referringSite || '(direct)';
+        if (referringSite in referrerCounts) {
+            referrerCounts[referringSite]++;
+        } else {
+            referrerCounts[referringSite] = 1;
+        }
+    }
+    return referrerCounts;
+}
+
+function getActiveUsers() {
+    return Object.keys(visitorsData).length;
+}
+
 //Start the server
-server.listen(serverPort, err => {
-    if (err)
-    {
+http.listen(serverPort, err => {
+    if (err) {
         console.error(err);
         return;
     }
     console.log('Server listening on port: ' + serverPort);
 });
+
