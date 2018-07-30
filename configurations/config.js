0be8d9a262112f31c327e39767c1f6f3cf66b658
ashen <jayasinghe.ashen@gmail.com>
2018-07-30 23:14:52 +0530

    openCV_Face_V1

--- configurations/config.js
+++ configurations/config.js
@@ -9,8 +9,8 @@ config.ffmpegPath = 'F:/Program Files/ffmpeg-20180706-cced03d-win64-static/bin/f
 config.JWTSECRET = 'rogueowlseverywhere';
 config.imageCopyPath = './public/allImages/';
 config.pythonRestUrl = 'http://127.0.0.1:5003/postdata';
-config.testPath = './controllers/opencv-face-recognition-python/test-data/test.jpg';
-config.imagesTrainingPath = './controllers/opencv-face-recognition-python/training-data/s';
+config.testPath = './opencv-face-recognition-python/test-data/test.jpg';
+config.imagesTrainingPath = './opencv-face-recognition-python/training-data/s';
 
 // Bluemix Watson speech to text
 config.bluemixSpeechToTextUrl = "https://stream.watsonplatform.net/speech-to-text/api";
