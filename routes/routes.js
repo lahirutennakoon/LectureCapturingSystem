1cf56521e3d2336668319ca31e63888dc57f1974
Vimukthi Mudalige <vimukthimudalige@gmail.com>
2018-09-26 11:50:12 +0530

    Recalibrate camera.

--- routes/routes.js
+++ routes/routes.js
@@ -10,12 +10,15 @@ router.use(bodyParser.json());
 // Import controllers
 const videoController_lt = require('../controllers/videoController_lt');
 
-
+const ptzController_vm = require('../controllers/ptzController_vm');
 
 // videoController_lt routes
 router.post('/api/video', videoController_lt.uploadVideo);
 router.get('/api/videos', videoController_lt.getAllVideos);
 router.post('/api/videoChapters', videoController_lt.createVideoChapters);
 router.get('/api/video', videoController_lt.getOneVideo);
+
+// ptzController routes
+router.get('/api/recalibrateCamera', ptzController_vm.recalibrateCamera);
 
 module.exports = router;
